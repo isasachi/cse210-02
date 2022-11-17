5adfde3c02224b3f48aa9f8adfaf45d7d0f989d6
Isaac Salirrosas <isaacsalirrosasc@gmail.com>
2022-11-16 19:49:25 -0500

    second commit

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -2,13 +2,30 @@ import random
 
 
 class Card:
+    """
+    A small piece of cardboard with a number that could go from 1 to 13 written in one of its corners.
+
+    Attributes:
+        value (int): The number written in the card's corner.
+        hl_value (string): The letter h, l or e.
+    """
 
     def __init__(self):
-        
+        """Constructs a new instance of Card with a value and a hl_value attribute.
+
+        Args:
+            self (Card): An instance of Card.
+        """
+
         self.value = random.randint(1, 13)
         self.hl_value = ""
     
     def update_value(self):
+        """Randomly chooses a new card value, compares it with the previous value to determine if it's higher or lower and updates the previous value with the new value.
+
+        Args:
+            self (Card): An instance of Card.
+        """
 
         new_value = random.randint(1, 13)
         if new_value > self.value:
--- hilo/game/controller.py
+++ hilo/game/controller.py
@@ -1,15 +1,34 @@
 from game.card import Card
 
 class Controller:
+    """
+    The Controller is responsible of controlling the sequence of play.
+
+    Attributes:
+        card (Card): A Card instance.
+        choice (string): The higher or lower choice of the player.
+        is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+    """
 
     def __init__(self):
+        """Constructs a new Controller.
         
+        Args:
+            self (Controller): an instance of Controller.
+        """
+
         self.card = Card()
         self.choice = ""
         self.is_playing = True
         self.score = 300
 
     def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Controller): an instance of Controller.
+        """
         
         while self.is_playing:
             self.show_card()
@@ -17,13 +36,23 @@ class Controller:
             self.play_again()
 
     def show_card(self):
+        """Shows the initial card value.
         
+        Args:
+            self (Controller): an instance of Controller.
+        """
+
         if not self.is_playing:
             return
 
         print(f"The card is: {self.card.value}")
 
     def update_game(self):
+        """Asks the player to choose higher or lower, then updates the card's value and the player's score.
+        
+        Args:
+            self (Controller): an instance of Controller.
+        """
 
         if not self.is_playing:
             return
@@ -46,6 +75,11 @@ class Controller:
             self.is_playing = False
 
     def play_again(self):
+        """Asks the player if he wants to start a new round.
+        
+        Args:
+            self (Controller): an instance of Controller.
+        """
 
         play_again = input("Play again? [y/n] ")
         if play_again.lower() == "y":
